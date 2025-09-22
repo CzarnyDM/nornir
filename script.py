f28e0c78bcdb3f5b0aca7601c8a993a7b4bd46ac
CzarnyDM <czarnydm@gmail.com>
2025-09-22 01:33:19 +0100

    Added MAC Address table seperation for Aruba

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from nornir.core.filter import F
 from nornir_napalm.plugins.tasks import napalm_get
 import json
 from napalm import get_network_driver
+from mac_vendor_lookup import MacLookup
 
 
 nr = InitNornir()
@@ -16,17 +17,36 @@ driver = get_network_driver("junos")
 def show_arp_napalm():
         result = nr.run(task=napalm_get, 
                         getters=["get_arp_table"], )
-        # print_result(result)
-        
-        mac_address = []
-        
+        print_result(result)
+
 
 def show_arp():
-        result = nr.run(
+        arp_json = []
+        filtered_hosts = nr.filter(F(groups__contains="aruba"))
+        result = filtered_hosts.run(
         task=netmiko_send_command,
         command_string="show arp",
-        use_textfsm=True
     )
+        print_result(result)
+        for host, task_result in result.items():
+                # task_result is a MultiResult object; first element has .result
+                output = task_result[0].result
+                for line in output.splitlines()[2:-2]:  # skip header and footer
+                        parts = line.split()
+                        if len(parts) >= 4:  # make sure line has all columns
+                                arp_json.append({
+                                "ip": parts[0],
+                                "mac": parts[1],
+                                "port": parts[2]
+                                })
+        arp_json = json.dumps(arp_json, indent=4)
+        print(arp_json)
+
+# def vendor_lookup():
+#         mac = MacLookup()
+#         vendor = mac.lookup(arp_json['mac'])
+#         print(vendor)
 
 
-show_arp_napalm()
+arp_json = show_arp()
+type(arp_json)
